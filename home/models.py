e99ed28aca25459432f532b4ec17c55f07e1df25
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-10-12 11:19:11 +0600

    Full project update: ERP_PROJECT – new apps, templates, settings

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name_of_project = models.CharField(max_length=255)
+    project_address = models.CharField(max_length=255)
+    contact_person_name = models.CharField(max_length=255)
+    contact_person_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
