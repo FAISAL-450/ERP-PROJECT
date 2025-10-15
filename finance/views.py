87fa2d90926bd925f43b9dd69fa73d1c4f29cc13
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-10-15 14:55:26 +0600

    Full project update: ERP_PROJECT – new apps, templates, settings

--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Account  # ✅ Corrected import
+from account.models import Account
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # 🔁 Reusable Pagination Function
