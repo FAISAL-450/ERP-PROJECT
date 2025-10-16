65b95f31c0f583e10e2e2e0eb5d40907f6c32af7
FAISAL-450 <faisalahmed400bc@gmail.com>
2025-10-16 18:08:55 +0600

    Full project update: ERP_PROJECT – new apps, templates, settings

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'account'
+
 urlpatterns = [
     path('dashboard/', views.account_dashboard, name='account_dashboard'),
     path('dashboard/edit/<int:pk>/', views.edit_account, name='edit_account'),
